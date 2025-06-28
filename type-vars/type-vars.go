e8a8f6bbf59d3ad7e14ec83b765bb23eda8a44ea
arikash117 <ari.kash117@gmail.com>
2025-06-28 10:34:43 +0300

    type vars

---
+++ type-vars/type-vars.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var releaseYear int = 2015
+	var model string = "Dodge Challenger SRT Hellcat"
+	var isMuscleCar bool = true
+
+	horsepowers := 717
+
+	fmt.Println("A year of release:", releaseYear)
+	fmt.Println("Model:", model)
+	fmt.Println("Is muscle car:", isMuscleCar)
+	fmt.Println("Horsepowers:", horsepowers)
+}
